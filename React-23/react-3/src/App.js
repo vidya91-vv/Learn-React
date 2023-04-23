21fa167d4ade2887184fc6fe1374d73fff3a7339
vidya91-vv <venvidyashree@gmail.com>
2023-04-23 12:37:15 +0530

    Counter1

--- React-23/react-3/src/App.js
+++ React-23/react-3/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
-import Message from './Message/Message'
-import Message1 from './Message/Message1'
+//import Message from './Message/Message'
+//import Message1 from './Message/Message1'
 import Counter from './Counter/Counter'
+import Counter1 from './Counter/Counter1'
 
 
  let App = () => {
   return (<div>
     <Navbar></Navbar>
-    <h1>React State Component</h1>
+    
 
     
-    <Message1 />
+    
     <Counter />
+    <Counter1 />
 
     <button className="btn btn-success">Test</button>
     </div>)

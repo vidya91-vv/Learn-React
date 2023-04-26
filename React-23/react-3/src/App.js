c6a962d211d634f4f7f269b64f8238552eb0b429
vidya91-vv <venvidyashree@gmail.com>
2023-04-26 23:40:53 +0530

    forms2

--- React-23/react-3/src/App.js
+++ React-23/react-3/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
 //import Login from './Form/Login'
-import Login1 from './Form/Login1'
+//import Login1 from './Form/Login1'
+import Login2 from './Form/Login2'
 
  let App = () => {
   return (<div>
     <Navbar />
     
-    <Login1 />
+    <Login2 />
 
     </div>)
   
